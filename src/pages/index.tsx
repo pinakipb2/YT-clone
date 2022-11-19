bb9155f01c4ea1c9e7ed79ee4c641eb7970022ec
Pinaki Bhattacharjee <pinakipb2@gmail.com>
2022-11-19 15:51:57 +0530

    Disable text select on smaller screen

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import Navbar from '../components/Navbar';
 
 const Home = () => {
   return (
-    <div className="flex flex-col bg-[#0F0F0F] h-screen scrollbar-thin scrollbar-thumb-[#CACACA] scrollbar-track-[#0F0F0F] overflow-y-scroll scrollbar-thumb-rounded-full scrollbar-thumb-h-[100px] pb-10 overflow-x-hidden scroll-smooth">
+    <div className="flex flex-col bg-[#0F0F0F] h-screen scrollbar-thin scrollbar-thumb-[#CACACA] scrollbar-track-[#0F0F0F] overflow-y-scroll scrollbar-thumb-rounded-full scrollbar-thumb-h-[100px] pb-10 overflow-x-hidden scroll-smooth select-none md:select-auto">
       {/* Navbar */}
       <Navbar />
       {/* Sidebar */}
